28e3d0d08c5f8023e96d604318cac008b4b15f2c
Alex-Picado <eidanpicadol@gmail.com>
2026-09-24 00:22:59 -0600

    Subida de arreglos

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:cubit_lab_desarrollo4/body_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'task_repository.dart';
 
 import 'state/task_cubit.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
